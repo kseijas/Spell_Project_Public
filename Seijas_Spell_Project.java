3b152c3a3b4d794f729240fe758b505e1e03d973
kseijas <46869411+kseijas@users.noreply.github.com>
2019-01-27 17:35:46 -0500

    Add files via upload
    This is the first load to remote GIT project Week 3

---
+++ Seijas_Spell_Project.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seijas_spell_project;
+
+
+/**
+ *
+ * @author Master1
+ */
+public class Seijas_Spell_Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // TODO code application logic here
+        Seijas_Main_Menu  seijas_main_menu  = new Seijas_Main_Menu ();
+        seijas_main_menu.setVisible(true);
+    }
+    
+}
